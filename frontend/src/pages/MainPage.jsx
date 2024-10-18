4bcc8f693e92aced35e1a4411d4b00be35d91fb5
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2024-10-18 13:30:53 +0300

    Start work with socket

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import useAuth from '../hooks/useAuth.jsx';
 import { fetchChannels, fetchMessages } from '../services/chat.js';
+import ChatForm from '../components/ChatForm.jsx';
 
 const MainPage = () => {
   const { user, logOut } = useAuth();
@@ -116,34 +117,7 @@ const MainPage = () => {
                     className="chat-messages overflow-auto px-5"
                     style={{ minHeight: '63vh' }}
                   />
-                  <div className="mt-auto px-5 py-3">
-                    <form noValidate className="py-1 border rounded-2">
-                      <div className="input-group has-validation">
-                        <input
-                          name="body"
-                          aria-label="Новое сообщение"
-                          placeholder="Введите сообщение..."
-                          className="border-0 p-0 ps-2 form-control"
-                          value=""
-                        />
-                        <button type="submit" disabled className="btn btn-group-vertical">
-                          <svg
-                            // xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            width="20"
-                            height="20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
-                            />
-                          </svg>
-                          <span className="visually-hidden">Отправить</span>
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+                  <ChatForm />
                 </div>
               </div>
             </div>
