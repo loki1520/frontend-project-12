51483c5302b0c15ff85e22efd6c698e299488634
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2024-10-28 13:53:10 +0300

    Check bug socket

--- frontend/src/modals/index.js
+++ frontend/src/modals/index.js
@@ -1,11 +1,11 @@
 import AddChannels from './AddChannels.jsx';
-import RemoveChannel from './RemoveChannel.jsx';
+// import RemoveChannel from './RemoveChannel.jsx';
 import RenameChannel from './RenameChannel.jsx';
 
 const modals = {
   adding: AddChannels,
   renaming: RenameChannel,
-  removing: RemoveChannel,
+  // removing: RemoveChannel,
 };
 
 const getModal = (modalType) => modals[modalType]; // return modal component
