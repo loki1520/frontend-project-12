4aac14027d156f407e0aca00a1d157007cfab498
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2024-10-31 00:11:53 +0300

    Add README

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ root.render(
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
         <App />
-        <ToastContainer autoClose={2000} pauseOnHover />
+        <ToastContainer autoClose={8000} pauseOnHover />
       </I18nextProvider>
     </Provider>
   </React.StrictMode>,
